f886bfe3a3b6bad011e12ad20f4e07a0227e44c9
Will Nickson <47941022+will-nickson@users.noreply.github.com>
2021-12-01 06:03:09 +0000

    store startTime data from FTX

--- data/btc_ohlc.py
+++ data/btc_ohlc.py
@@ -12,16 +12,18 @@ db_connection = mysql.connect(
 
 cursor = db_connection.cursor()
 
-historical_data = requests.get('https://ftx.com/api/markets/BTC/USD/candles?resolution=86400').json()
-# print(historical_data)
+response = requests.get('https://ftx.com/api/markets/BTC/USD/candles?resolution=86400').json()
+# print(response)
+data = response['result']
+# print(data)
 
 def insert_data(data):
-    for i in data['result']:
-        sql = 'insert into btcusd (time, open, high, low, close, volume) values (%s, %s, %s, %s, %s, %s)'
-        val = (i['time'], i['open'], i['high'], i['low'], i['close'], i['volume'])
+    for i in data:
+        sql = 'insert into btcusd (start_time, time, open, high, low, close, volume) values (%s, %s, %s, %s, %s, %s, %s)'
+        val = (i['startTime'], i['time'], i['open'], i['high'], i['low'], i['close'], i['volume'])
         cursor.execute(sql, val)
 
     db_connection.commit()
     print(cursor.rowcount, "records inserted...")
 
-insert_data(historical_data)
+insert_data(data)
