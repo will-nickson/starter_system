460b6255a1fcfc2b53629791b615e15485c0ff16
Will Nickson <47941022+will-nickson@users.noreply.github.com>
2021-12-15 06:41:33 +0000

    revert mysql connection

--- data/btc_ohlc.py
+++ data/btc_ohlc.py
@@ -1,20 +1,30 @@
+import os
 import sys
 import requests
+import mysql.connector as mysql
 from datetime import datetime
-sys.path.insert(0, './')
-import mysql_conn 
 
-cursor = mysql_conn.connect()
 ftx_data = requests.get('https://ftx.com/api/markets/BTC/USD/candles?resolution=86400').json()
+# print(ftx_data)
+
+conn = mysql.connect(
+    host=os.environ.get('MYSQL_HOST'),
+    user=os.environ.get('MYSQL_USER'),
+    password=os.environ.get('MSQL_PASSWORD'),
+    database=os.environ.get('BTC_DATA')
+)
+
+cursor = conn.cursor()
 
 def get_mysql_data():
     cursor.execute('select start_time from btcusd order by start_time desc limit 1;')
     result = cursor.fetchall()
+    print(result)
     return result[0][0]
 
 recent = get_mysql_data()
 
-def get_data_to_insert():
+def format_ftx_data():
     d = []
     sorted_data = sorted(ftx_data['result'], key=lambda x: x['time'], reverse=True)
 
@@ -26,15 +36,15 @@ def get_data_to_insert():
 
     return d
 
-new_data = get_data_to_insert()
+new_data = format_ftx_data()
 
-def insert_data(data):
-    for i in data:
+def insert_data(new_data):
+    for i in new_data:
         sql = 'insert into btcusd (start_time, time, open, high, low, close, volume) values (%s, %s, %s, %s, %s, %s, %s)'
         val = (i['startTime'], i['time'], i['open'], i['high'], i['low'], i['close'], i['volume'])
         cursor.execute(sql, val)
 
-    db_connection.commit()
+    conn.commit()
     print(cursor.rowcount, "records inserted...")
 
 insert_data(new_data)
