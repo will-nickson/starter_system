419f78988e8ac7105ba30985f1a63ea246bd26fc
Will Nickson <47941022+will-nickson@users.noreply.github.com>
2022-01-14 07:49:32 +0000

    move simple_system

---
+++ simple_system.py
@@ -0,0 +1,34 @@
+# import data 
+
+from systems.forecasting import Rules
+# import System
+# import ForecastCombine
+# import ForecastScaleCap
+# import PositionSizing
+# import Portfolios
+# import Account
+# import RawData
+
+
+def simple_system(data=None, config=None, log_level="on"):
+    """
+        The most simple trading system possible
+    """
+
+    my_system = System(
+        [
+            Account(),
+            Portfolios(),
+            PositionSizing(),
+            ForecastCombine(),
+            ForecastScaleCap(),
+            Rules(),
+            RawData()
+        ],
+        data,
+        config
+    )
+
+    my_system.set_logging_level(log_level)
+
+    return my_system
